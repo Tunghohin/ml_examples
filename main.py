dd9e8ebf5afb87e735cd1c1117c5ec8004002702
Tunghohin <houhin0809@gmail.com>
2024-05-01 14:26:24 +0800

    Update: add dataset

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.axes as axes
+from sklearn.model_selection import train_test_split
+import math
+import torch
+import seaborn
 
-if __name__ == '__main__':
-    x = np.arange(30, dtype=np.float16).reshape(3, 5, 2)
-    print(x.dtype)
+if __name__ == "__main__":
